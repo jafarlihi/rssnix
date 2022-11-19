6cde34ff8da04f27ac84abb8e2cfecd0ab6548e7
Hikmat Jafarli <jafarlihi@gmail.com>
2022-11-20 00:03:22 +0400

    Add stuff

--- feed.go
+++ feed.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	log "github.com/sirupsen/logrus"
+)
+
 type Feed struct {
 	Name    string
 	Address string
+}
+
+func UpdateAllFeeds() {
+	log.Info("All feeds updated")
+}
+
+func UpdateFeed(feed string) {
+	log.Info(feed + " updated")
 }
--- main.go
+++ main.go
@@ -1,10 +1,58 @@
 package main
 
 import (
+	"errors"
+	"os"
+	"os/exec"
+
 	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	LoadConfig()
-	log.Info("30 articles fetched from blah")
+
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Usage:   "opens the config file with $EDITOR",
+				Action: func(cCtx *cli.Context) error {
+					editor, ok := os.LookupEnv("EDITOR")
+					if len(editor) == 0 || !ok {
+						return errors.New("$EDITOR environment variable is not set")
+					}
+					homePath, err := os.UserHomeDir()
+					if err != nil {
+						log.Error("Failed to get home path")
+						os.Exit(1)
+					}
+					cmd := exec.Command(editor, homePath+"/.config/rssnix/config.ini")
+					cmd.Stdin = os.Stdin
+					cmd.Stdout = os.Stdout
+					err = cmd.Run()
+					return err
+				},
+			},
+			{
+				Name:    "update",
+				Aliases: []string{"u"},
+				Usage:   "update given feed(s) or all feeds if no argument is given",
+				Action: func(cCtx *cli.Context) error {
+					if cCtx.Args().Len() == 0 {
+						UpdateAllFeeds()
+					}
+					for i := 0; i < cCtx.Args().Len(); i++ {
+						UpdateFeed(cCtx.Args().Get(i))
+					}
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
